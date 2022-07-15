ee44661218741c1b574f8fb530cc79a592f5c6e6
EhsonZokirov <zokirov_2000@mail.ru>
2022-07-16 01:10:44 +0500

    cimmitNew

--- classWork/task11.js
+++
@@ -1,4 +0,0 @@
-let N = 3
-let K = 14
-let result = Math.floor(K / N)
-console.log(result);
---
+++ classWork/tasks(15,07,22).js
@@ -0,0 +1,7 @@
+// let str='     ab      '
+// console.log(str.repeat(6));    //// sixбор стринга такрор када нишон мета
+
+// console.log(str.trimEnd());    //// ай охир спейсора убрать мена
+
+// console.log(Math.min(1,2,3));  //// хуртарин адада нишон мета
+
